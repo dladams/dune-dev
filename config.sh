18070860fae8f5f5fb0ae5d7904f6793ff93bb5c
David Adams <dladams@bnl.gov>
2015-09-16 10:10:08 -0400

    Add DUNE_MAXPROC.

--- config.sh
+++ config.sh
@@ -14,8 +14,11 @@ DUNE_PROJECTVERSION=v04_21_01
 
 # Specify the UPS qualifier.
 # UPS can be used to discover the available values, e.g.
-# > ups list -aK+ | grep larsoft | grep v02_03_00
+# > ups list -aK+ larsoft | grep v02_03_00
 DUNE_QUAL=e7:prof
 
 # Flag for which increasing values provide more log messages.
 DUNE_VERBOSE=1
+
+# Number of processes to use for dune build.
+DUNE_MAXPROC=4
