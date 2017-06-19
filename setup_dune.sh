1a1e7c39653363753f2b5168d1faff0a1200ce0c
David Adams <dladams@bnl.gov>
2017-06-19 13:22:20 -0400

    Twiddle.

--- setup_dune.sh
+++ setup_dune.sh
@@ -1,3 +1,12 @@
+# setup_dune.sh
+#
+# Generic product setup file for dune.
+#
+# Goal is to define the PRODUCTS path and setup the ups command.
+
 echo Setting up DUNE UPS products for $DUNE_SITE
-if [ -n "$DUNE_VERBOSE" ]; then echo Setting up ups; fi
-source $PRODUCTS/setups
+PRODDIR=`echo $PRODUCTS | sed 's/.*://g'`
+PRODSETUP=$PRODDIR/setups
+if [ -n "$DUNE_VERBOSE" ]; then echo Setting up ups with $PRODSETUP; fi
+source $PRODSETUP
+echo Done
