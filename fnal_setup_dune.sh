f6517d6220dd4eb5d95f4bf3b7673e5f77aaafa0
David Adams <dladams@bnl.gov>
2020-02-12 08:33:27 -0600

    Change init?

--- fnal_setup_dune.sh
+++ fnal_setup_dune.sh
@@ -1,5 +1,9 @@
 echo Setting up PRODUCTS for FNAL
-source /grid/fermiapp/products/setups.sh
-source /grid/fermiapp/products/larsoft/setup
-source /grid/fermiapp/products/dune/setup_dune.sh
+if true; then
+  source /grid/fermiapp/products/setups.sh
+  source /grid/fermiapp/products/larsoft/setup
+  source /grid/fermiapp/products/dune/setup_dune.sh
+else
+  source /cvmfs/fermilab.opensciencegrid.org/products/larsoft/setup
+fi
 echo PRODUCTS=$PRODUCTS
